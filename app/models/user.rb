94d08d5a617688118073cc7673002c9840ffc7c4
Rachel Loziuk <rachel.loziuk@gmail.com>
2013-11-22 11:03:10 -0600

    added devise to user Model; added links to application view for sign in , registration, and signout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessible :username, :email
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  attr_accessible :username, :email, :password, :password_confirmation
 end
--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Force your application to not access the DB or load models when precompiling your assets (deploying Rails 3.1 on Heroku)
-config.assets.initialize_on_precompile = false
+
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -35,6 +34,9 @@ module Readit
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Force your application to not access the DB or load models when precompiling your assets (deploying Rails 3.1 on Heroku)
+    config.assets.initialize_on_precompile = false
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
