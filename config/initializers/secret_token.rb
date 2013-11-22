26f0f433bb4ec0a9b0a109f5d519a3e6b4316341
Rachel Loziuk <rachel.loziuk@gmail.com>
2013-11-22 09:08:43 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Readit::Application.config.secret_token = '5278c8ceb8daf33eddcf49dbb512300d2d810e6495d8fc57fb8dca3fad953facd83db4da828c62b312371f11fadc4552a3e6b6f08d153b14b4fb287a0a5dffbf'
