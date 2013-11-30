1f08cc82f57aff8d53db4c5568134da553d86a8f
Rachel Loziuk <rachel.loziuk@gmail.com>
2013-11-30 11:20:27 -0600

    added links show and create actions; added links index view

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,14 +1,23 @@
 class LinksController < ApplicationController
+	def index
+		@links = Link.all
+	end
+
 	def show
-	
+		@link = Link.find(params[:id])
 	end
 
 	def new
 		@link = Link.new
-
 	end
 
 	def create
+		@link = Link.new(params[:link])
+		if @link.save
+			redirect_to @link
+		else
+			render action: "new"
+		end 
 
 	end
 end
