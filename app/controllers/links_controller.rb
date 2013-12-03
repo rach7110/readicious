2d4e6461f718ec20d1ca5bbe2f0eb82d625d8d94
Rachel Loziuk <rachel.loziuk@gmail.com>
2013-12-03 11:03:59 -0600

    added Link edit and update controller actions and views

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -14,9 +14,22 @@ class LinksController < ApplicationController
 	def create
 		@link = Link.new(params[:link])
 		if @link.save
-			redirect_to @link
+			redirect_to @link, notice: 'Arcticle was successfully added!'
 		else
 			render action: "new"
 		end 
+	end
+
+	def edit
+		@link = Link.find(params[:id])
+	end
+
+	def update
+		@link = Link.find(params[:id])
+		if @link.update_attributes(params[:link])
+			redirect_to @link, notice: "Article was successfully updated!"
+		else
+			render action: "edit"
+		end
 	end
 end
