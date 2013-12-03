0c042615999edd7f59dc97facb303f6fe9985e24
Rachel Loziuk <rachel.loziuk@gmail.com>
2013-12-03 10:42:15 -0600

    added a current_page? helper to application view so 'Home' link doesnt' show on the homepage

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -12,12 +12,11 @@ class LinksController < ApplicationController
 	end
 
 	def create
-		render text: params[:link].inspect
-		# @link = Link.new(params[:link])
-		# if @link.save
-		# 	redirect_to @link
-		# else
-		# 	render action: "new"
-		# end 
+		@link = Link.new(params[:link])
+		if @link.save
+			redirect_to @link
+		else
+			render action: "new"
+		end 
 	end
 end
