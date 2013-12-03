fcac274c8c022bf0180e7a302f3e270984cc0a94
Rachel Loziuk <rachel.loziuk@gmail.com>
2013-12-03 13:32:59 -0600

    added a comment controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,11 @@
+class CommentsController < ApplicationController
+# require a user to be logged in before running create action:
+before_filter :authenticate_user!, :only => [:create]
+
+	def create
+		@comment = Comment.create(params[:comment])
+		 @comment = current_user.comments.create(params[:comment].merge(link_id: params[:link_id]))
+		# When a comment is created, redirect the user back to the same view:
+		redirect_to :back
+	end
+end
